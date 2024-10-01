4ac0c231acfc37a4350a807909e246d1d4ef3dec
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-10-01 11:32:32 -0300

    feat: create bucket on workspace

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket = "rhyzzor-bucket"
+  bucket = "rhyzzor-bucket-${terraform.workspace}"
 
   tags = {
     name = "RhyzzorBucket"
     Iac = true
+    context = "${terraform.workspace}"
   }
 }
